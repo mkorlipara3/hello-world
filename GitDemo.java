235df71538ff55007d8e84a4ba32638d350dfb4a
mkorlipara3 <79949448+mkorlipara3@users.noreply.github.com>
2021-03-02 19:44:41 -0700

    Update GitDemo.java

--- GitDemo.java
+++ GitDemo.java
@@ -4,6 +4,7 @@
 public class GitDemo {
     
     private double previousNumber;
+    
     private double reverseNum;
     
     public GitDemo() {
@@ -14,8 +15,9 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
-        //System.out.print("Add num to previousNumber");
-        return num + previousNumber;
+        previousNumber = previousNumber + num;
+        System.out.print("Add num to previousNumber");
+        return 0;
     }
     
     /*****
@@ -23,24 +25,25 @@ public class GitDemo {
      * i.e. if num = 4,
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
-    public double findFactorial(int num){
-        double newNumber = 1;
-        reverseNum = newNumber;
+    public double findFactorial(int num) {
+       double newNumber = 1;
+        
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
         
+        reverseNum = newNumber;
+   
         return newNumber;
     }
-    
     
     /*****
      * This function should reverse 'num' and return the resulting value.
      * i.e. if num = 1234
      *  the function will return 4321
      *****/
-    public double reverseNumber(int num) {
-        double factorialNumber = 0;
+    public double reverseNumber(int num){
+         double factorialNumber = 0;
         
         while( num != 0 ) {
             double temp = num % 10;
