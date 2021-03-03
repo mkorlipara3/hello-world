e50a65b40eaadd1e8d5439da994c66e557c5f9e3
mkorlipara3 <79949448+mkorlipara3@users.noreply.github.com>
2021-03-02 19:23:02 -0700

    Update GitDemo.java

--- GitDemo.java
+++ GitDemo.java
@@ -4,7 +4,6 @@
 public class GitDemo {
     
     private double previousNumber;
-    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,8 +13,8 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
-        //System.out.print("Add num to previousNumber");
-        return num + previousNumber;
+        System.out.print("Add num to previousNumber");
+        return 0;
     }
     
     /*****
